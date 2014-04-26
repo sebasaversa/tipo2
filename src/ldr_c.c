651b1a49ec3e56de02100733b43f7d562398dfb5
Tomas <tomas@tomas-HP-Pavilion-g6-Notebook-PC.(none)>
2014-04-26 18:56:38 -0300

    LRD

--- src/ldr_c.c
+++ src/ldr_c.c
@@ -24,7 +24,53 @@ void ldr_c    (
         {
             rgb_t *p_d = (rgb_t*) &dst_matrix[i][j * 3];
             rgb_t *p_s = (rgb_t*) &src_matrix[i][j * 3];
-            *p_d = *p_s;
+            if(i < 2 || j < 2 || i+2 > filas || j+2 > cols)
+            {
+				*p_d = *p_s;
+			}
+			else
+			{
+					LDR(p_d, p_s, filas, alta);
+			}
         }
     }
+}
+
+void LDR(rgb_t* p_d, rgb_t* p_s, int f, int alfa){
+	unsigned sumTotal = 0;
+	unsigned sumColores = 0;
+	unsigned int max = 5 * 5 * 255 * 3 * 255;
+	
+	
+	unsigned int r = sumaColor(&p_s->r, f);
+	unsigned int g = sumaColor(&p_s->g, f);
+	unsigned int b = sumaColor(&p_s->b, f);
+	
+	sumColores = r + g + b;
+	r *= sumColores*alfa;
+	g *= sumColores*alfa;
+	b *= sumColores*alfa;
+	r /= max;
+	g /= max;
+	b /= max;
+	p_d->r = max(p_d->r + r  , 0);
+	p_d->g = max(p_d->g + g  , 0);
+	p_d->b = max(p_d->b + b  , 0);
+	p_d->r = min(p_d->r + r  , 255);
+	p_d->g = min(p_d->g + g  , 255);
+	p_d->b = min(p_d->b + b  , 255);
+	
+}
+
+unsigned int sumaColor(unsigned int* r, int f){
+	unsigned int suma = 0;
+	&r -= (6 - (filas*3*2));
+	
+	for( int c = 0; f < 5; f++){
+		&r += (filas*3);
+		for( int f = 0; f < 5*3; f+= 3){
+			suma += *(&r+f); // suma el valor del rojo en la posicion
+		}
+	}
+	return suma;
 }
