f895b6082dcfa1ed6259a1363944ba208827a21b
seba <sebastianaversa@gmail.com>
2014-04-26 00:33:09 -0300

    popart_c..c

--- src/popart_c.c
+++ src/popart_c.c
@@ -14,8 +14,8 @@ void popart_c    (
 	int cols,
 	int filas,
 	int src_row_size,
-	int dst_row_size)
-{
+	int dst_row_size){
+	
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
@@ -23,10 +23,17 @@ void popart_c    (
 		for (int j_d = 0, j_s = 0; j_d < cols; j_d++, j_s++) {
 			rgb_t *p_d = (rgb_t*)&dst_matrix[i_d][j_d*3];
 			rgb_t *p_s = (rgb_t*)&src_matrix[i_s][j_s*3];
-			*p_d = *p_s;
-		}
-	}
-
-}
-
-
+			int s = temperatura(p_s->r, p_s->g, p_s->b);
+			*p_d = colores[s];}}}
+			
+int temperatura(unsigned int r, unsigned int g, unsigned int b){
+	unsigned int suma = r + g + b;
+	int res;
+	
+	if 		 (suma < 153)	{res = 0;}
+	else if (suma < 306)	{res = 1;}
+	else if (suma < 459)	{res = 2;}
+	else if (suma < 612)	{res = 3;}
+	else 					{res = 4;}
+	
+	return res;}
